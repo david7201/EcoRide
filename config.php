06304f6cbe9a67df05998517ebdbb1b0ea13d872
samoc2023 <145985857+samoc2023@users.noreply.github.com>
2024-03-18 22:26:44 +0000

    Update config.php

--- config.php
+++ config.php
@@ -6,7 +6,7 @@
 $host = "localhost";
 $username = "root";
 $password = "pass";
-$dbname = "ecoride"; // will use later
+$dbname = "ecorides"; // will use later
 $dsn = "mysql:host=$host;dbname=$dbname"; // will use later
 $options = array(
  PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
